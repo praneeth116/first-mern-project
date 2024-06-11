fdb4a313829c6a8aa5b3fb105c9b8557d220e8de
praneeth116 <136475805+Praneeth1189@users.noreply.github.com>
2024-06-11 19:17:42 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+//Defining user schemas for username, password and email.
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        }
+    }, {timestamps: true}
+)
+
+//Name of the model must be uppercase
+const User = mongoose.model('User', userSchema);
+
+export default User;
