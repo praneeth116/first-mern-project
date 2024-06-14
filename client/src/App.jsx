99c29ec13b0b69cf39ef31b4a69d19c7834d527a
praneeth116 <136475805+Praneeth1189@users.noreply.github.com>
2024-06-14 12:50:37 +0530

    update header component and made profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signin from './pages/Signin.jsx'
 import SignUp from './pages/SignUp.jsx'
 import Profile from './pages/Profile.jsx'
 import Header from './components/Header.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 function App() {
   return( 
@@ -16,7 +17,9 @@ function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/sign-in" element={<Signin/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+      <Route element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile/>}/>
+      </Route>
     </Routes>
   </BrowserRouter>)
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 function Header() {
+  const {currentUser} = useSelector((state)=>state.user)
   return (
     <div className='bg-slate-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -10,14 +12,21 @@ function Header() {
             </Link>
             <ul className='flex gap-6 hover:text-red'>
                 <Link to="/">
-                    Home
+                    <li>Home</li>
                 </Link>
                 <Link to="/about">
-                    About
+                <li>About</li>
                 </Link>
-                <Link to="/sign-in">
-                    Sign In
-                </Link>
+                {currentUser ? (
+                    <Link to="/profile">
+                        <img src={currentUser.profilePicture} alt="profile"
+                        className='h-7 w-7 rounded-full object-cover'/>
+                    </Link>
+                ):(
+                    <Link to="/sign-in">
+                    <li>Sign in</li>
+                    </Link>
+                )}
             </ul>
         </div>
     </div>
